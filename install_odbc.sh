935c8cd0b5a73bf5054a85a9df94c1959a282517
Manuj Rai <imanujrai7@gmail.com>
2025-04-09 13:48:45 +0530

    modified:   install_odbc.sh

--- install_odbc.sh
+++ install_odbc.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# Update system
-apt-get update
+# Update and install dependencies
+apt-get update && apt-get install -y curl gnupg apt-transport-https unixodbc unixodbc-dev
 
-# Install ODBC prerequisites
-apt-get install -y curl gnupg apt-transport-https unixodbc unixodbc-dev
-
-# Add Microsoft repository keys
+# Add Microsoft GPG key
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 
-# Add Microsoft SQL Server ODBC repo
+# Add Microsoft SQL Server repository (for Debian 11)
 curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
-# Update and install the driver
-apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql17
+# Update and install the SQL Server ODBC driver
+apt-get update && ACCEPT_EULA=Y apt-get install -y msodbcsql17
